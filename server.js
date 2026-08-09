9dc53836bdf6d1b1b9e22a74688c69cc7c90b9cd
widialjatsiyah <widi.aljatsiyah@gmail.com>
2026-08-10 03:53:36 +0700

    (*) update mysql

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '172.0.0.1',
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD ,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'iseeyou',
   waitForConnections: true,
   connectionLimit: 10,
