f4ad4fde34b6fe142668eea63f9e6ad0efb6ec91
widialjatsiyah <widi.aljatsiyah@gmail.com>
2026-08-10 03:58:39 +0700

    (*) update mysql2

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.json({ limit: '20mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '172.0.0.1',
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER || 'root',
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'iseeyou',
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
