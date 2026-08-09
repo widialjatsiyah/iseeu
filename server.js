15f3ece56b4f2a9d1098c7ce177aea776fc389fd
widialjatsiyah <widi.aljatsiyah@gmail.com>
2026-08-10 04:52:09 +0700

    (*) update serverjs3

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ console.log({
 });
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
+  host: '153.92.15.54',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: 'u912091910_widi',
   password: 'Hanyawidi390',
